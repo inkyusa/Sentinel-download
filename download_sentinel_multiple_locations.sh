1416c7324fef4e522055f565c74939f52e0a9966
Inkyu <inkyu.sa@csiro.au>
2021-03-17 10:49:27 +1000

    added switch option (directory to store downloaded files)

--- download_sentinel_multiple_locations.sh
+++ download_sentinel_multiple_locations.sh
@@ -1,9 +1,28 @@
 #!/bin/bash
 
+get_abs_filename() {
+  echo "$(cd "$(dirname "$1")" && pwd)/$(basename "$1")"
+}
+
+helpFunction()
+{
+   echo "-w [dir path to store data]"
+   exit 1
+}
+
+
+while getopts "w:" opt
+do
+   case "$opt" in
+      w ) w="$OPTARG" ;;
+      ? ) helpFunction ;;
+   esac
+done
+#w=$(get_abs_filename "$w")
 i=0
 while IFS=, read -r lat lon
 do
     test $i -eq 0 && ((i=i+1)) && continue
     echo "lat = $lat, lon = $lon"
-    python Sentinel_download.py --lat $lat --lon $lon -a apihub.txt -m 10 -d 20210301 -f 20210312 -s S2
+    python Sentinel_download.py --lat $lat --lon $lon -a apihub.txt -m 10 -d 20210301 -f 20210312 -s S2 -w "$w"
 done < locations.csv
