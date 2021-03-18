762d75f941255b62c91591891f72d346cd41af7f
Inkyu <inkyu.sa@csiro.au>
2021-03-18 13:44:55 +1000

    added single location downloader and another location csv file

--- download_sentinel2_multiple_locations.sh
+++ download_sentinel2_multiple_locations.sh
@@ -1,28 +1,36 @@
 #!/bin/bash
 
-get_abs_filename() {
-  echo "$(cd "$(dirname "$1")" && pwd)/$(basename "$1")"
-}
-
 helpFunction()
 {
    echo "-w [dir path to store data]"
    exit 1
 }
 
+usageFunction()
+{
+   echo "============================================================="
+   echo "Usage:"
+   echo "./download_sentinel2_multiple_locations.sh -w ./dataset -f locations.csv"
+   echo "-w dir to store data"
+   echo "-f csv_file that contains (lat, log) location per row"
+   echo "============================================================="
+}
 
-while getopts "w:" opt
+usageFunction
+
+while getopts "w:f:" opt
 do
    case "$opt" in
       w ) w="$OPTARG" ;;
+      f ) f="$OPTARG" ;;
       ? ) helpFunction ;;
    esac
 done
-#w=$(get_abs_filename "$w")
+echo $f
 i=0
 while IFS=, read -r lat lon
 do
     test $i -eq 0 && ((i=i+1)) && continue
     echo "lat = $lat, lon = $lon"
     python Sentinel_download.py --lat $lat --lon $lon -a apihub.txt -m 5 -d 20210301 -f 20210312 -s S2 -w "$w"
-done < locations2.csv
+done < $f
--- download_sentinel2_single_location.sh
+++ download_sentinel2_single_location.sh
@@ -1,40 +1,43 @@
 #!/bin/bash
 
-get_abs_filename() {
-  echo "$(cd "$(dirname "$1")" && pwd)/$(basename "$1")"
-}
-
 helpFunction()
 {
-   echo "-w [dir path to store data]"
+   usageFunction
    exit 1
 }
 
 usageFunction()
 {
-   echo "==================================================="
+   echo "============================================================="
    echo "Usage:"
-   echo "./download_sentinel2_single_location.sh -a -27.1 -b 157.1 -w ./dataset"
-   echo "-a Latitude"
-   echo "-b Longitude"
+   echo "./download_sentinel2_single_location.sh -w ./dataset -l "12.5, 40.2""
    echo "-w dir to store data"
-   echo "==================================================="
+   echo "-l Latitude, Longitude"
+   echo "!!!! -l option argument should be wrapped with " " !!!!!!" 
+   echo "============================================================="
 }
 
 
-while getopts "w:a:b:" opt
+while getopts "w:l:" opt
 do
    case "$opt" in
       w ) w="$OPTARG" ;;
-      a ) a="$OPTARG" ;;
-      b ) b="$OPTARG" ;;
+      l ) l="$OPTARG" ;;
       ? ) helpFunction ;;
    esac
 done
-lat=$a
-lon=$b
+
+IFS=', ' read -r -a loc <<< $l
+lat=${loc[0]}
+lon=${loc[1]}
+
 usageFunction
-#w=$(get_abs_filename "$w")
-echo "lat = $lat, lon = $lon"
+
+echo "lat = ${lat}, lon = ${lon}"
 echo "Data will be saved $w"
 python Sentinel_download.py --lat $lat --lon $lon -a apihub.txt -m 5 -d 20210301 -f 20210312 -s S2 -w "$w"
+
+
+#echo "Latitude = $lat, Longitute = $lon"
+#echo "Data will be saved $w"
+#python Sentinel_download.py --lat $lat --lon $lon -a apihub.txt -m 5 -d 20210301 -f 20210312 -s S2 -w "$w"
